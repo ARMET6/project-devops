2dfe0231a0561774a8b428255d008606e814ba5b
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-03-30 19:50:26 +0000

    feat: compresión de archivos a tar.gz

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -12,3 +12,6 @@ FECHA=$(date +%Y%m%d_%H%M%S)
 ARCHIVO_ZIP="backup_${FECHA}.tar.gz"
 LOG_FILE="logs/backup.log"
 
+# Comprimir archivos
+echo "[${FECHA}] Iniciando respaldo del directorio ${DIRECTORIO}" >> $LOG_FILE
+tar -czf $ARCHIVO_ZIP $DIRECTORIO 2>> $LOG_FILE
