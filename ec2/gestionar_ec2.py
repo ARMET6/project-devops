4a0ee05e02faed271b99e831c31d2d6bb8164bcd
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-03-30 19:20:13 +0000

    feat: listar instancias EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -8,3 +8,10 @@ def main():
 
     accion = sys.argv[1]
     ec2 = boto3.client('ec2', region_name='us-east-1')
+
+    try:
+        if accion == "listar":
+            response = ec2.describe_instances()
+            for reservation in response['Reservations']:
+                for instance in reservation['Instances']:
+                    print(f"ID: {instance['InstanceId']} - Estado: {instance['State']['Name']}")
